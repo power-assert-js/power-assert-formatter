ae0f1ad02f0e9dc715bfe43ae6935f6c43333679
Takuto Wada <takuto.wada@gmail.com>
2013-12-25 03:10:25 +0900

    Generate RequireJS config from bower.json using grunt-bower-requirejs.

---
+++ test/rjsconfig.js
@@ -0,0 +1,21 @@
+var require = {
+    paths: {
+        assert: "../bower_components/assert/assert",
+        empower: "../bower_components/empower/lib/empower",
+        "es5-shim": "../bower_components/es5-shim/es5-shim",
+        escodegen: "../bower_components/escodegen/escodegen",
+        espower: "../bower_components/espower/lib/espower",
+        esprima: "../bower_components/esprima/esprima",
+        estraverse: "../bower_components/estraverse/estraverse",
+        mocha: "../bower_components/mocha/mocha",
+        requirejs: "../bower_components/requirejs/require"
+    },
+    shim: {
+        assert: {
+            exports: "assert"
+        },
+        escodegen: {
+            exports: "escodegen"
+        }
+    }
+};
