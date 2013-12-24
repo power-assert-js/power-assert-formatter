19765a5221c31795754c9f0307ee07307995f0e7
Takuto Wada <takuto.wada@gmail.com>
2013-12-25 02:19:21 +0900

    Test options_test.js on Browser (not automated yet).

--- test/options_test.js
+++ test/options_test.js
@@ -1,12 +1,44 @@
-var createFormatter = require('../lib/power-assert-formatter'),
-    empower = require('empower'),
-    weave = require('../test_helper').weave,
-    baseAssert = require('assert');
+(function (root, factory) {
+    'use strict';
+
+    var dependencies = [
+        '../lib/power-assert-formatter',
+        'empower',
+        '../test_helper',
+        'assert'
+    ];
+
+    function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+    function toLowerCamel(str) {
+        var tokens = str.split(/[-_]/);
+        return tokens[0] + tokens.slice(1).map(function (token) { return capitalize(token); }).join('');
+    }
+
+    if (typeof define === 'function' && define.amd) {
+        define(dependencies, factory);
+    } else if (typeof exports === 'object') {
+        factory.apply(root, dependencies.map(function (path) { return require(path); }));
+    } else {
+        factory.apply(root, dependencies.map(function (path) {
+            var tokens = path.split('/'),
+                basename = tokens[tokens.length - 1];
+            return root[toLowerCamel(basename)];
+        }));
+    }
+}(this, function (
+    createFormatter,
+    empower,
+    testHelper,
+    baseAssert
+) {
+
+var weave = testHelper.weave;
 
 suite('lineSeparator option', function () {
     function lineSeparatorTest (name, option, expectedSeparator) {
-        var baseAssert = require('assert'),
-            assert = empower(baseAssert, createFormatter(option));
+        var assert = empower(baseAssert, createFormatter(option));
         test(name, function () {
             var falsyNum = 0;
             try {
@@ -29,3 +61,5 @@ suite('lineSeparator option', function () {
     lineSeparatorTest('CR', {lineSeparator: '\r'}, '\r');
     lineSeparatorTest('CRLF', {lineSeparator: '\r\n'}, '\r\n');
 });
+
+}));
