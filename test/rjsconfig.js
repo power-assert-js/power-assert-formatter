c257a0e5cf97fbc842447193948dc5547806db11
Takuto Wada <takuto.wada@gmail.com>
2013-12-25 03:15:50 +0900

    Tests on browser with RequireJS (not automated yet).

--- test/rjsconfig.js
+++ test/rjsconfig.js
@@ -3,7 +3,7 @@ var require = {
         assert: "../bower_components/assert/assert",
         empower: "../bower_components/empower/lib/empower",
         "es5-shim": "../bower_components/es5-shim/es5-shim",
-        escodegen: "../bower_components/escodegen/escodegen",
+        escodegen: "../bower_components/escodegen/escodegen.browser",
         espower: "../bower_components/espower/lib/espower",
         esprima: "../bower_components/esprima/esprima",
         estraverse: "../bower_components/estraverse/estraverse",
