fc16f174f9c9900d09dd5d8490c9723d1f85ec84
Takuto Wada <takuto.wada@gmail.com>
2014-04-16 18:44:15 +0900

    Add jsondiffpatch to provide more descriptive diff.

--- test/rjsconfig.js
+++ test/rjsconfig.js
@@ -8,7 +8,8 @@ var require = {
         esprima: "../bower_components/esprima/esprima",
         estraverse: "../bower_components/estraverse/estraverse",
         mocha: "../bower_components/mocha/mocha",
-        requirejs: "../bower_components/requirejs/require"
+        requirejs: "../bower_components/requirejs/require",
+        jsondiffpatch: "../bower_components/jsondiffpatch/build/bundle"
     },
     shim: {
         assert: {
