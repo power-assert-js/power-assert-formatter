99d118bfe090b164c02391d1951f31ed84bce49c
Takuto Wada <takuto.wada@gmail.com>
2014-05-11 15:29:11 +0900

    Export and test constructorNameOf function

---
+++ test/types_and_values_test.js
@@ -0,0 +1,110 @@
+(function (root, factory) {
+    'use strict';
+
+    var dependencies = [
+        '../lib/power-assert-formatter',
+        'assert'
+    ];
+
+    function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+    function toLowerCamel(str) {
+        var tokens = str.split(/[-_]/);
+        return tokens[0] + tokens.slice(1).map(function (token) { return capitalize(token); }).join('');
+    }
+
+    if (typeof define === 'function' && define.amd) {
+        define(dependencies, factory);
+    } else if (typeof exports === 'object') {
+        factory.apply(root, dependencies.map(function (path) { return require(path); }));
+    } else {
+        factory.apply(root, dependencies.map(function (path) {
+            var tokens = path.split('/'),
+                basename = tokens[tokens.length - 1];
+            return root[toLowerCamel(basename)];
+        }));
+    }
+}(this, function (
+    formatter,
+    assert
+) {
+    var constructorNameOf = formatter.constructorNameOf;
+
+    suite('constructorNameOf', function () {
+
+        suite('primitives', function () {
+            test('string', function () {
+                assert.equal(constructorNameOf('hoge'), 'String');
+            });
+            test('number', function () {
+                assert.equal(constructorNameOf(5), 'Number');
+            });
+            test('boolean', function () {
+                assert.equal(constructorNameOf(false), 'Boolean');
+            });
+            test('regexp literal', function () {
+                assert.equal(constructorNameOf(/^not/), 'RegExp');
+            });
+            test('function', function () {
+                assert.equal(constructorNameOf(function () {}), 'Function');
+            });
+        });
+
+        suite('global objects', function () {
+            test('undefined throws TypeError', function () {
+                assert.throws(function () {
+                    constructorNameOf(undefined);
+                });
+            });
+            test('null throws TypeError', function () {
+                assert.throws(function () {
+                    constructorNameOf(null);
+                });
+            });
+            test('NaN', function () {
+                assert.equal(constructorNameOf(NaN), 'Number');
+            });
+            test('Infinity', function () {
+                assert.equal(constructorNameOf(Infinity), 'Number');
+            });
+        });
+
+        suite('wrapper objects', function () {
+            test('String', function () {
+                assert.equal(constructorNameOf(new String('hoge')), 'String');
+            });
+            test('Number', function () {
+                assert.equal(constructorNameOf(new Number('1')), 'Number');
+            });
+            test('Boolean', function () {
+                assert.equal(constructorNameOf(new Boolean('1')), 'Boolean');
+            });
+            test('Date', function () {
+                assert.equal(constructorNameOf(new Date()), 'Date');
+            });
+            test('RegExp', function () {
+                assert.equal(constructorNameOf(new RegExp('^not', 'g')), 'RegExp');
+            });
+        });
+
+        suite('user-defined constructor', function () {
+            test('named', function () {
+                function Person(name, age) {
+                    this.name = name;
+                    this.age = age;
+                }
+                assert.equal(constructorNameOf(new Person('bob', 5)), 'Person');
+            });
+            test('anonymous', function () {
+                var Person = function(name, age) {
+                    this.name = name;
+                    this.age = age;
+                };
+                assert.equal(constructorNameOf(new Person('bob', 5)), 'Object');
+            });
+        });
+
+    });
+
+}));
