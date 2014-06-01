5632a9c4b90c512686cf703788f6d0332606c4a8
Takuto Wada <takuto.wada@gmail.com>
2014-06-01 22:26:44 +0900

    Extract string-width.js out

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ var estraverse = require('estraverse'),
     DiffMatchPatch = require('googlediff'),
     defaultStringifier = require('./lib/stringify'),
     defaultComparator = require('./lib/comparator'),
+    multibyteStringWidthOf = require('./lib/string-width'),
     EsNode = require('./lib/esnode'),
     syntax = estraverse.Syntax;
 
@@ -221,18 +222,6 @@ function rightToLeft (a, b) {
 }
 
 
-function multibyteStringWidthOf (str) {
-    var i, c, width = 0;
-    for(i = 0; i < str.length; i+=1){
-        c = str.charCodeAt(i);
-        if ((0x0 <= c && c < 0x81) || (c === 0xf8f0) || (0xff61 <= c && c < 0xffa0) || (0xf8f1 <= c && c < 0xf8f4)) {
-            width += 1;
-        } else {
-            width += 2;
-        }
-    }
-    return width;
-}
 
 
 // borrowed from qunit.js
