60e7fc72377b19d1b72aa48038f225474ab7f12d
Takuto Wada <takuto.wada@gmail.com>
2015-04-18 16:40:03 +0900

    chore(power-assert-formatter): use empower npm module on browser too

--- test/rjsconfig.js
+++ test/rjsconfig.js
@@ -1,7 +1,7 @@
 var require = {
     paths: {
         assert: "../bower_components/assert/assert",
-        empower: "../bower_components/empower/build/empower",
+        empower: "../node_modules/empower/build/empower",
         acorn: '../node_modules/acorn/dist/acorn',
         escodegen: '../bower_components/escodegen/escodegen.browser',
         espower: "../node_modules/espower/build/espower",
